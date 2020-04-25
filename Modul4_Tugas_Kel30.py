7910cdd1012988903f46b00fa6f6de148eca4c41
ilhamdaffa21 <64187539+ilhamdaffa21@users.noreply.github.com>
2020-04-25 22:57:42 +0700

    Update

--- Modul4_Tugas_Kel30.py
+++ Modul4_Tugas_Kel30.py
@@ -1,39 +1,24 @@
 import metod
-def buatpassword ():
-        opin=0
-        opin2=1
-        knt=0
-        while opin != opin2:
-            if knt > 0:
-               print("Password dan Password Konfirmasi Berbeda")
-            knt=1
-            print("Masukkan password : ")
-            opin = input()
-            print("Masukkan password konfirmasi : ")
-            opin2 = input()
-        return(opin)
-        print("Login berhasil")
-
+p = metod.huhaha()
 print("Buat User 1")
 user1 = input()
 print("Buat Password 1")
-pin1=buatpassword()
+pin1=p.buatpassword()
 print("Buat User 2")
 user2 = input()
 print("Buat Password 2")
-pin2=buatpassword()
+pin2=p.buatpassword()
 
 print("Login")
-print(user1)
-print(user2)
+print("1", user1)
+print("2", user2)
 print("Input Username")
 user = input()
-p = metod.huhaha(user, pin1, pin2)
 if user == user1:
-    p.ulang1()
+    p.ulang1(pin1)
     
 else:
-    p.ulang2()
+    p.ulang2(pin2)
 
-p.mulai()
+p.mulai(user)
 p.selesai(10)
