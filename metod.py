ab845947f8fbb49f578e90dad9aae9b22f5a94a0
ilhamdaffa21 <64187539+ilhamdaffa21@users.noreply.github.com>
2020-04-25 22:58:35 +0700

    Update

--- metod.py
+++ metod.py
@@ -1,38 +1,44 @@
 class huhaha:
-    cpin1=0
-    cpin2=0
-    cuser=0
-    def __init__(self, user, pin1, pin2):
-        self.cuser = user
-        self.cpin1 = pin1
-        self.cpin2 = pin2
-    def mulai(self):
-        print(f"Selamat Datang {self. cuser}")
+    def mulai(self,user):
+        print(f"Selamat Datang {user}")
     def selesai(self, waktu):
         print("Loading")
         while waktu > 0:
                 print(waktu)
                 waktu -= 1
-    def ulang1 (self):
-        vpin=self.cpin1 + "1"
+    def ulang1 (self,pin1):
+        vpin= pin1 + "1"
         knt=0
-        while vpin != self.cpin1:
+        while vpin != pin1:
             if knt > 0:
                print("Password salah")
             knt=1
             print("Masukkan password")
             vpin = input()
         print("Login berhasil")
-    def ulang2 (self):
-        vpin=self.cpin1 + "1"
+    def ulang2 (self,pin2):
+        vpin=pin2 + "1"
         knt=0
-        while vpin != self.cpin2:
+        while vpin != pin2:
             if knt > 0:
                print("Password salah")
             knt=1
             print("Masukkan password")
             vpin = input()
         print("Login berhasil")
-        
-        
+    def buatpassword (self):
+        opin=0
+        opin2=1
+        knt=0
+        while opin != opin2:
+            if knt > 0:
+               print("Password dan Password Konfirmasi Berbeda")
+            knt=1
+            print("Masukkan password : ")
+            opin = input()
+            print("Masukkan password konfirmasi : ")
+            opin2 = input()
+        return(opin)
+        print("Login berhasil")
+
 
