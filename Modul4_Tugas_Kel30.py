89299efd061cf4ae9d1d31d118b13948669f2bbf
Benediktus Gilang <57548941+luxur1a@users.noreply.github.com>
2020-04-25 21:34:09 +0700

    Update Modul4_Tugas_Kel30.py
    user1 sama 2 nya gua buat variabel biar bisa ganti nickname

--- Modul4_Tugas_Kel30.py
+++ Modul4_Tugas_Kel30.py
@@ -4,21 +4,23 @@ pw1 = input()
 print("Buat password User 2")
 pw2 = input()
 print("Login")
-print("User1(1)")
-print("User2(2)")
+user1 = "User1"
+user2 = "User2"
+print("(1)", user1)
+print("(2)", user2)
 print("Input 1 atau 2")
 user = int(input())
 if user == 1:
-    print("Masukkan password User1")
+    print("Masukkan password ", user1)
     pw = input()
     while pw != pw1:
             print("Password salah")
             print("Masukkan password")
             pw = input()
-            print("Login berhasil")
+    print("Login berhasil")
     
 else:
-    print("Masukkan password User2")
+    print("Masukkan password ", user2)
     pw = input()
     while pw != pw2:
             print("Password salah")
