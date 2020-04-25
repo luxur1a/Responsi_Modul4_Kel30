1088c8dbb9ea692d70c7d89d37cdd7900e930950
ilhamdaffa21 <64187539+ilhamdaffa21@users.noreply.github.com>
2020-04-25 22:08:20 +0700

    Update

--- Modul4_Tugas_Kel30.py
+++ Modul4_Tugas_Kel30.py
@@ -1,32 +1,38 @@
 import metod
-print("Buat passsword User 1")
-pw1 = input()
-print("Buat password User 2")
-pw2 = input()
+def buatpassword ():
+        opin=0
+        opin2=1
+        knt=0
+        while opin != opin2:
+            if knt > 0:
+               print("Password dan Password Konfirmasi Berbeda")
+            knt=1
+            print("Masukkan password : ")
+            opin = input()
+            print("Masukkan password konfirmasi : ")
+            opin2 = input()
+        return(opin)
+        print("Login berhasil")
+
+print("Buat User 1")
+user1 = input()
+print("Buat Password 1")
+pin1=buatpassword()
+print("Buat User 2")
+user2 = input()
+print("Buat Password 2")
+pin2=buatpassword()
+
 print("Login")
-user1 = "User1"
-user2 = "User2"
 print("(1)", user1)
 print("(2)", user2)
 print("Input 1 atau 2")
-user = int(input())
-if user == 1:
-    print("Masukkan password ", user1)
-    pw = input()
-    while pw != pw1:
-            print("Password salah")
-            print("Masukkan password")
-            pw = input()
-    print("Login berhasil")
+user = input()
+p = metod.huhaha(user, pin1, pin2)
+if user == user1:
+    p.ulang1()
     
 else:
-    print("Masukkan password ", user2)
-    pw = input()
-    while pw != pw2:
-            print("Password salah")
-            print("Masukkan password")
-            pw = input()
-            print("Login berhasil")
-p = metod.huhaha("user")
+    p.ulang2()
 p.mulai()
 p.selesai(10)
