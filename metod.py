ce942f1ea3a31e5076cce5326d398480b3e710e1
Benediktus Gilang <57548941+luxur1a@users.noreply.github.com>
2020-04-25 21:29:16 +0700

    Update metod.py
    buat switch case.. nanti gua buat ganti user name aj

--- metod.py
+++ metod.py
@@ -3,6 +3,13 @@ class huhaha:
         self.user = user
     def mulai(self):
         print(f"Selamat Datang {self. user}")
+        options = {
+	        1 : 'Senin', #ganti nickname user
+	        2 : 'Selasa', #ganti password (koko)
+	    }
+        x = float(input('Seting data pengguna'))
+        print('Hari : %s' % options.setdefault(x, 'Inputan hanya 1 - 7'))
+
     def selesai(self, waktu):
         print("Loading")
         while waktu > 0:
