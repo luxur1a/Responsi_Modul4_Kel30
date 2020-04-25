de6d5bffb261c17ee23c0d8e5a60e86bb157b0fa
Benediktus Gilang <57548941+luxur1a@users.noreply.github.com>
2020-04-25 20:04:45 +0700

    Create metod.py

---
+++ metod.py
@@ -0,0 +1,12 @@
+class huhaha:
+    def __init__(self, user):
+        self.user = user
+    def mulai(self):
+        print(f"Selamat Datang {self. user}")
+    def selesai(self, waktu):
+        print("Loading")
+        while waktu > 0:
+                print(waktu)
+                waktu -= 1
+        
+
