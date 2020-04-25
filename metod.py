af3fd9a22065cb94c5775e1e32ef4eece449ce49
ilhamdaffa21 <64187539+ilhamdaffa21@users.noreply.github.com>
2020-04-25 22:09:16 +0700

    Update

--- metod.py
+++ metod.py
@@ -1,19 +1,38 @@
 class huhaha:
-    def __init__(self, user):
-        self.user = user
+    cpin1=0
+    cpin2=0
+    cuser=0
+    def __init__(self, user, pin1, pin2):
+        self.cuser = user
+        self.cpin1 = pin1
+        self.cpin2 = pin2
     def mulai(self):
-        print(f"Selamat Datang {self. user}")
-        options = {
-	        1 : 'Senin', #ganti nickname user
-	        2 : 'Selasa', #ganti password (koko)
-	    }
-        x = float(input('Seting data pengguna\n1 = Ganti nama user\n2 = Ganti password'))
-        print('Hari : %s' % options.setdefault(x, 'Inputan hanya 1 - 7'))
-
+        print(f"Selamat Datang {self. cuser}")
     def selesai(self, waktu):
         print("Loading")
         while waktu > 0:
                 print(waktu)
                 waktu -= 1
+    def ulang1 (self):
+        vpin=self.cpin1 + "1"
+        knt=0
+        while vpin != self.cpin1:
+            if knt > 0:
+               print("Password salah")
+            knt=1
+            print("Masukkan password")
+            vpin = input()
+        print("Login berhasil")
+    def ulang2 (self):
+        vpin=self.cpin1 + "1"
+        knt=0
+        while vpin != self.cpin2:
+            if knt > 0:
+               print("Password salah")
+            knt=1
+            print("Masukkan password")
+            vpin = input()
+        print("Login berhasil")
+        
         
 
